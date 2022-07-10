d2e4af131b0595aae67f2a7e2ba69c47ef21d907
Daniel Filipe Nunes Silva <danielementary@proton.me>
2022-07-10 20:16:20 +0200

    add simple structure of taskbook, boards, tasks and notes

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+type task struct {
+	id          uint
+	description string
+	completed   bool
+}
+
+type note struct {
+	id          uint
+	description string
+}
+
+type taskbook struct {
+	number_of_boards uint
+	boards           []*board
+}
+
+type board struct {
+	id    uint
+	name  string
+	tasks []*task
+	notes []*note
+}
+
+func (tb *taskbook) newBoard(name string) {
+	var id = tb.number_of_boards
+	tb.number_of_boards++
+
+	var tasks = []*task{}
+	var notes = []*note{}
+
+	var board = board{id, name, tasks, notes}
+
+	tb.boards = append(tb.boards, &board)
+}
+
 func main() {
+	var tb = taskbook{number_of_boards: 0, boards: []*board{}}
 	fmt.Println("Taskbook opened!")
+
+	tb.newBoard("Coding")
 }
